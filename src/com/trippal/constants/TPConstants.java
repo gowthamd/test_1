6c734cd96f1c3d366a6d149599c52b20c8ef333b
syed96 <syedtouhid8@gmail.com>
2017-01-03 19:04:12 +0530

    test commit1

--- src/com/trippal/constants/TPConstants.java
+++ src/com/trippal/constants/TPConstants.java
@@ -5,7 +5,10 @@ package com.trippal.constants;
  * Class to hold all the constants
  *
  */
-public class TPConstants {
+public class TPConstants
+{
 	public static final String GOOGLE_API_KEY = "GOOGLE_API_KEY";
 	public static final String GOOGLE_AUTOCOMPLETE_API = "https://maps.googleapis.com/maps/api/place/autocomplete/json";
+
+	int v = 2;
 }
